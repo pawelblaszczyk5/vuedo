96306b5a964f57e7fab86cd52dfa55c059fb8f60
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-08-31 10:51:39 +0200

    feat: pinia setup

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
 import App from '~/App.vue';
 import '@unocss/reset/tailwind.css';
 import 'virtual:uno.css';
 
-createApp(App).mount('#app');
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.mount('#app');
