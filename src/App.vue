0a0cd5f1b92d16d0846270adca5dc0401e6f7967
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-08-31 10:44:41 +0200

    feat: add TodoForm and base behaviour

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
+<script lang="ts" setup>
+	import TodoForm from '~/components/TodoForm.vue';
+	import TodoList from '~/components/TodoList.vue';
+</script>
+
 <template>
-	<main class="flex flex-col items-center justify-center gap-2 p-4">
-		<h1 class="text-4xl">Hello world</h1>
-		<i class="i-carbon-accessibility text-4xl text-blue-400" />
+	<main class="flex flex-col items-center justify-center gap-4 p-8">
+		<h1 class="font-200 flex items-center gap-2 text-4xl">
+			TODOs
+			<i
+				aria-hidden
+				class="i-bi-card-checklist text-emerald-400"
+			/>
+		</h1>
+		<TodoForm />
+		<TodoList />
 	</main>
 </template>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+	let todoText = $ref('');
+
+	const handleSubmit = () => {
+		const trimmedTodoText = todoText.trim();
+
+		if (!trimmedTodoText) return;
+
+		console.log(trimmedTodoText);
+		todoText = '';
+	};
+</script>
+
+<template>
+	<form
+		class="flex w-full max-w-xs flex-col"
+		novalidate
+		@submit.prevent="handleSubmit"
+	>
+		<label
+			for="todo-text"
+			class="pb-1 text-sm"
+			>Todo text</label
+		>
+		<div class="flex">
+			<input
+				id="todo-text"
+				v-model="todoText"
+				class="w-full border-2 border-emerald-400 bg-zinc-800 p-2 text-sm focus-visible:border-fuchsia-400 focus-visible:outline-none"
+				name="todo-text"
+				type="text"
+				required
+			/>
+			<button
+				class="bg-emerald-400 px-6 text-zinc-800 ring-inset ring-fuchsia-400 focus-visible:outline-none focus-visible:ring-2"
+			>
+				Add
+			</button>
+		</div>
+	</form>
+</template>
