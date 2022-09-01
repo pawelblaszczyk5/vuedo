f7dbdb06d82621c1a41cd1425c2aeb8b0cddec9c
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-09-01 20:43:44 +0200

    feat: initial todoStore structure

---
+++ src/utils/todoStore.ts
@@ -0,0 +1,39 @@
+import { acceptHMRUpdate, defineStore } from 'pinia';
+import { v4 as uuid } from 'uuid';
+
+interface Todo {
+	text: string;
+	id: string;
+	createdAt: number;
+	status: 'completed' | 'uncompleted';
+}
+
+type TodoId = Todo['id'];
+type TodoText = Todo['text'];
+type TodoStatus = Todo['status'];
+
+export const useTodoStore = defineStore('counter', () => {
+	const todos = $ref<Array<Todo>>([]);
+
+	const createTodo = (text: TodoText) =>
+		void todos.push({
+			text,
+			id: uuid(),
+			createdAt: Date.now(),
+			status: 'uncompleted',
+		});
+
+	const changeTodoStatus = (idToChange: TodoId, newStatus: TodoStatus) => {
+		const todoToModify = todos.find(({ id }) => idToChange === id);
+
+		if (!todoToModify) return;
+
+		todoToModify.status = newStatus;
+	};
+
+	return { todos: $$(todos), createTodo, changeTodoStatus };
+});
+
+if (import.meta.hot) {
+	import.meta.hot.accept(acceptHMRUpdate(useTodoStore, import.meta.hot));
+}
